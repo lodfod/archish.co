78671df1a1a56e2e2db7fc68e3eb009a67bacede
archish-edu <archisharun99@gmail.com>
2023-04-10 18:31:45 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MainView from './views/MainView'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="font-rasa">
+      <MainView />
+    </div>
+  )
+}
+
+export default App
---
+++ src/views/MainView.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function MainView() {
+    return (
+        <div className='items-center sm:h-screen text-center content-center p-20 text-4xl font-medium sm:flex justify-center sm:justify-between '>
+            <span className=''>
+                archish arun
+
+            </span>
+
+            <div className='pt-2 flex flex-col sm:flex-row justify-center items-center '>
+                <div>
+                    <div className='h-36 w-36 pt-5 flex justify-center'>
+                        <img className=" h-fit  align-middle grayscale hover:grayscale-0" src="/src/assets/archish_flamingo.png" alt="" />
+                    </div>
+
+                </div>
+
+                <div>
+                    <div className='h-36 w-36 flex justify-center align-middle'>
+                        <img className=" h-36 grayscale hover:grayscale-0" src="/src/assets/archish_keyboard.png" alt="" />
+                    </div>
+
+                </div>
+
+                <div>
+                    <div className='h-44 w-44 flex justify-center align-middle'>
+                        <img className="h-fit grayscale hover:grayscale-0 pt-6" src="/src/assets/archish_52prime.png" alt="" />
+                    </div>
+
+                </div>
+
+
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MainView
