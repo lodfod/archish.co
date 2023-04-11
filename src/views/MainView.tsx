56606bee6f81d623dfabb67fa158cd8efc6e5cb0
archish-edu <archisharun99@gmail.com>
2023-04-10 18:38:13 -0700

    image filepaths deployment

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -11,21 +11,21 @@ function MainView() {
             <div className='pt-2 flex flex-col sm:flex-row justify-center items-center '>
                 <div>
                     <div className='h-36 w-36 pt-5 flex justify-center'>
-                        <img className=" h-fit  align-middle grayscale hover:grayscale-0" src={"../../public/archish_flamingo.png"} alt="" />
+                        <img className=" h-fit  align-middle grayscale hover:grayscale-0" src={"/public/archish_flamingo.png"} alt="" />
                     </div>
 
                 </div>
 
                 <div>
                     <div className='h-36 w-36 flex justify-center align-middle'>
-                        <img className=" h-36 grayscale hover:grayscale-0" src={"../../public/archish_keyboard.png"} alt="" />
+                        <img className=" h-36 grayscale hover:grayscale-0" src={"/public/archish_keyboard.png"} alt="" />
                     </div>
 
                 </div>
 
                 <div>
                     <div className='h-44 w-44 flex justify-center align-middle'>
-                        <img className="h-fit grayscale hover:grayscale-0 pt-6" src={"../../public/archish_52prime.png"} alt="" />
+                        <img className="h-fit grayscale hover:grayscale-0 pt-6" src={"/public/archish_52prime.png"} alt="" />
                     </div>
 
                 </div>
