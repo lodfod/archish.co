5f48276a8df0b3fd44f735ceeda7f1f045265f8b
archish-edu <archisharun99@gmail.com>
2024-01-20 23:47:33 -0800

    fuck

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MainView from './views/MainView'
-
+import GeneralView from './views/GeneralView'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,9 +20,13 @@ function App() {
  
 
   return (
-    <div className="font-rasa">
-      <MainView />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div className='font-rasa'><MainView/></div>} />
+      <Route path="/general" element={<GeneralView/>} />
+   </Routes>
+    </BrowserRouter>
+   
   )
 }
 
